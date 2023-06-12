f5b2ba6a7799d936152cd36ff05cfce48a4008a4
kabirmadale <kabirmadale1996@gmail.com>
2023-06-12 20:56:50 +0530

    Adding category Entity till now..

---
+++ src/main/java/com/bikkadit/electronicstore/model/Category.java
@@ -0,0 +1,30 @@
+package com.bikkadit.electronicstore.model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+
+    @Id
+    private String categoryId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    private String coverImage;
+}
