cb74f002dd30ad1394c8d503953d30f2314cb810
kabirmadale <kabirmadale1996@gmail.com>
2023-06-16 12:33:06 +0530

    changes in appconstant is done till now..

--- src/main/java/com/bikkadit/electronicstore/helper/AppConstant.java
+++ src/main/java/com/bikkadit/electronicstore/helper/AppConstant.java
@@ -3,6 +3,11 @@ package com.bikkadit.electronicstore.helper;
 public class AppConstant {
     public static final String EXCEPTION_MSG ="User not found with given id ....";
     public static final String USER_DELETE ="User Deleted Successfully";
-    public static final String USER_EMAIL ="User not  found with emailId";
+    public static final String USER_EMAIL ="User not found with emailId";
+
+
+    public static final String CATEGORY_DELETE ="Category Deleted Successfully";
+    public static final boolean CATEGORY_STATUS =true;
+
 }
 
