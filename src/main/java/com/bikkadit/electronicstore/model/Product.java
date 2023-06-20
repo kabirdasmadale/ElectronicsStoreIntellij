277adfdfd95f41e8fa91a14932db739461aedacb
kabirmadale <kabirmadale1996@gmail.com>
2023-06-20 13:02:05 +0530

    Adding column name on Product entity class...

--- src/main/java/com/bikkadit/electronicstore/model/Product.java
+++ src/main/java/com/bikkadit/electronicstore/model/Product.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Date;
 
@@ -16,14 +18,23 @@ import java.util.Date;
 @Entity
 @Table(name="product")
 public class Product {
+    @Id
     private String productId;
+    @Column(name="product_title")
     private String title;
+    @Column(name="product_descripsition")
     private String descripsition;
+    @Column(name="product_price")
     private int price;
+    @Column(name="product_discountprice")
     private int discountPrice;
+    @Column(name="product_quantity")
     private String quantity;
+    @Column(name="product_added_date")
     private Date addedDate;
+    @Column(name="product_live")
     private boolean live;
+    @Column(name="product_stock")
     private boolean stotck;
 
 }
